9b7847ce0b01b81dbae2465d6d0a8c24869cebff
Ochieng424 <ochiengderrick424@gmail.com>
2020-01-26 20:01:00 +0300

    find()

---
+++ string_find_method.py
@@ -0,0 +1,2 @@
+my_book = "I love coding in Laravel"
+print(my_book.find('Laravel'))
--- string_title_method.py
+++ string_title_method.py
@@ -1,3 +1,5 @@
 name = "derrick ocHieng"
 
 print(name.title())
+
+# Capitalizes the first letter of each word in a string
