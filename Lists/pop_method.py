07c6d76d05d6201e7349d18d2eabf80c3c544368
Ochieng424 <ochiengderrick424@gmail.com>
2020-01-28 16:06:41 +0300

    pop(), pop(x)

---
+++ Lists/pop_method.py
@@ -0,0 +1,11 @@
+subscribers = ['a@gmail.com', 'b@gmail.com', 'c@gmail.com', 'd@gmail.com']
+
+# remove first element of a list
+popped = subscribers.pop()
+print(popped)
+print(subscribers)
+
+# remove element from a list using index
+popped = subscribers.pop(0)
+print(popped)
+print(subscribers)
