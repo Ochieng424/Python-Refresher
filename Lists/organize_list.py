03211589c33cf9808fe12679b7e480a13ce7d509
Ochieng424 <ochiengderrick424@gmail.com>
2020-01-28 16:39:44 +0300

    sort(reverse=True)

--- Lists/organize_list.py
+++ Lists/organize_list.py
@@ -3,3 +3,7 @@ months = ['jan', 'feb', 'march', 'april', 'may']
 # Sort list in alphabetical order
 months.sort()
 print(months)
+
+# Sort list in reversed alphabetical order
+months.sort(reverse=True)
+print(months)
