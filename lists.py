a8c46d83bc4aea57457b63039ebcf72d94a95243
Ochieng424 <ochiengderrick424@gmail.com>
2020-01-28 15:54:40 +0300

    list manipulation

---
+++ lists.py
@@ -0,0 +1,13 @@
+months = ['jan', 'feb', 'march']
+
+# add to the end of a list
+months.append('april')
+print(months)
+
+# add at a certain index of a list
+months.insert(0, 'may')
+print(months)
+
+# delete from list using index
+del months[0]
+print(months)
