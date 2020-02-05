bdd67546de6d9e49d141a9a57b79ba008814afba
Ochieng424 <ochiengderrick424@gmail.com>
2020-02-05 13:27:17 +0300

    add key, value to dictionary

--- Dictionaries/dict.py
+++ Dictionaries/dict.py
@@ -1,7 +1,12 @@
-book_1 = {
+user = {
     'name': 'derrick',
     'email': 'derrick@gmail.com'
 }
 
 # get values using keys
-print(book_1['name'])
+print(user['name'].title())
+
+# add key, value to dictionary
+user['phone'] = '0712345678'
+
+print(user['phone'])
